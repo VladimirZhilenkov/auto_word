07aa060d8072f17102e600b25c646cad2acb0d68
UlianaZhilenkova <UlianaZhilenkova@users.noreply.github.com>
2026-02-25 14:16:42 +0700

    bump: version 2.4.5

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,5 @@ Document Generator Application
 Automated Word document generation from templates with personal data.
 """
 
-__version__ = "2.4.2"
+__version__ = "2.4.5"
 __author__ = "Document Generator Team"
